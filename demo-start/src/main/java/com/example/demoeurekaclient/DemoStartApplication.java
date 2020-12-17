d8fba5807c045d63ceddfc31b0f28d6faa4cdf65
wb-zlr386384 <wb-zlr386384@alibabapictures.com>
2020-12-17 17:19:58 +0800

    提交单机zookeeper的文件

--- demo-start/src/main/java/com/example/demoeurekaclient/DemoStartApplication.java
+++ demo-start/src/main/java/com/example/demoeurekaclient/DemoStartApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -21,7 +22,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
-@SpringBootApplication(scanBasePackages = "com.example.demoeurekaclient")
+@SpringBootApplication(scanBasePackages = "com.example.demoeurekaclient" )
 @EnableEurekaClient
 @EnableCircuitBreaker       // 启用Hystrix
 //@EnableHystrixDashboard   // 启用Hystrix的控制面板
@@ -34,7 +35,6 @@ import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 @EnableBinding({Source.class , SourceDome.class})
 // 在一个接口中可以同时定义多个Binding，只需要定义多个@Input或@Output标注的方法。
 // Processor接口同时继承了Source和Sink接口，所以当@EnableBinding指定了Processor接口时相当于同时应用了两个Binding
-
 @EnableScheduling //开启定时任务
 public class DemoStartApplication {
 
